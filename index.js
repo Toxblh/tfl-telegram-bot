04f94ca50ffae4a11b887d2706fea9aca624f6a8
Anton Palgunov <toxblh@gmail.com>
2020-01-06 10:35:18 +0000

    add themes clipper info.

--- index.js
+++ index.js
@@ -1,44 +1,109 @@
-const axios = require('axios')
-const R = require('ramda')
-const dayjs = require('dayjs')
-const TelegramBot = require('node-telegram-bot-api')
-const { toHHMMSS, sortByTime, getNameStation, getPlatformNumber } = require('./helpers')
+const axios = require("axios");
+const R = require("ramda");
+const dayjs = require("dayjs");
+const TelegramBot = require("node-telegram-bot-api");
+const {
+  toHHMMSS,
+  sortByTime,
+  getNameStation,
+  getPlatformNumber
+} = require("./helpers");
 
-const STATIONS = require('./stations')
-const { TOKEN } = require('./token')
+const { STATIONS, PIERS } = require("./stations");
+const { TOKEN } = require("./token");
 
-const options = { polling: true }
-const bot = new TelegramBot(TOKEN, options)
+const options = { polling: true };
+const bot = new TelegramBot(TOKEN, options);
 
 STATIONS.map(station => {
   bot.onText(new RegExp(`/${station.command}`), msg => {
-    const chatId = msg.chat.id
+    const chatId = msg.chat.id;
 
     axios
       .get(`https://api.tfl.gov.uk/StopPoint/${station.stopPoint}/arrivals`)
       .then(({ data }) => {
-        const dd = sortByTime(data)
-        let output = ''
+        const dd = sortByTime(data);
+        let output = "";
 
         dd.map(item => {
           if (R.any(R.equals(item.platformName))(station.platforms)) {
-            const expectedArrival = dayjs(item.expectedArrival)
-            const timeToArrival = dayjs() > expectedArrival ? '0:00' : expectedArrival.diff(dayjs(), 'seconds')
+            const expectedArrival = dayjs(item.expectedArrival);
+            const timeToArrival =
+              dayjs() > expectedArrival
+                ? "0:00"
+                : expectedArrival.diff(dayjs(), "seconds");
 
-            output += '```\n'
-            output += getPlatformNumber(item.platformName)
-            output += ' | ' + toHHMMSS(timeToArrival)
-            output += ' | ' + getNameStation(item.destinationName)
-            output += '\n```'
+            output += "```\n";
+            output += getPlatformNumber(item.platformName);
+            output += " | " + toHHMMSS(timeToArrival);
+            output += " | " + getNameStation(item.destinationName);
+            output += "\n```";
           }
-        })
+        });
 
-        bot.sendMessage(chatId, output, { parse_mode: 'Markdown' })
-      })
-  })
-})
+        if (output === "") {
+          output = "No data";
+        }
+
+        bot.sendMessage(chatId, output, { parse_mode: "Markdown" });
+      });
+  });
+});
+
+PIERS.map(station => {
+  bot.onText(new RegExp(`/${station.command}$`), msg => {
+    const chatId = msg.chat.id;
+
+    axios
+      .get(`https://api.tfl.gov.uk/StopPoint/${station.stopPoint}/arrivals`)
+      .then(({ data }) => {
+        const dd = sortByTime(data);
+
+        let output = "";
+
+        dd.map(item => {
+          if (R.any(R.equals(item.direction))(station.direction)) {
+            const expectedArrival = dayjs(item.expectedArrival);
+            let md;
+            let liveTimeToArrival = expectedArrival.diff(dayjs(), "seconds");
+            const timeToArrival =
+              dayjs() > expectedArrival ? "0:00" : liveTimeToArrival;
+
+            liveTimeToArrival <= station.timeToStation && (md = "~");
+            liveTimeToArrival > station.timeToStation && (md = "_");
+            liveTimeToArrival > station.timeToStation + 180 && (md = "");
+
+            output = item.lineName;
+            // output += " | " + item.direction;
+            // output += " | " + dayjs(item.expectedArrival).format("HH:mm:ss");
+            // output += " -> " + dayjs(item.timeToLive).format("HH:mm:ss");
+            // output += " | " + toHHMMSS(item.timeToStation);
+            output += " | " + md + toHHMMSS(timeToArrival) + md;
+
+            output += " | " + getNameStation(item.destinationName);
+            // output += " | " + dayjs().format("HH:mm:ss");
+            // output += " | " + toHHMMSS(dayjs().diff(item.timestamp, "seconds"));
+          }
+        });
+
+        if (output === "") {
+          output = "No data";
+        }
+
+        bot.sendMessage(chatId, output, { parse_mode: "Markdown" });
+      });
+  });
+});
 
 bot.onText(/\/help/, msg => {
-  const chatId = msg.chat.id
-  bot.sendMessage(chatId, STATIONS.map(station => `/${station.command} - ${station.name}`).join('\n'))
-})
+  const chatId = msg.chat.id;
+
+  const stations = STATIONS.map(
+    station => `/${station.command} - ${station.name}\nFilters: ${station.platforms.join(', ')}\n`
+  ).join("\n");
+  const piers = PIERS.map(
+    station => `/${station.command} - ${station.name}\nFilters: ${station.direction.join(', ')}\n`
+  ).join("\n");
+
+  bot.sendMessage(chatId, `${stations}\n${piers}`);
+});
--- stations.js
+++ stations.js
@@ -1,58 +1,79 @@
 const STATIONS = [
-    {
-      name: 'Poplar',
-      command: 'pop',
-      stopPoint: '940GZZDLPOP',
-      platforms: ['Platform 1']
-    },
-    {
-      name: 'Ponton Dock',
-      command: 'pdk',
-      stopPoint: '940GZZDLPDK',
-      platforms: ['Platform 2']
-    },
-    {
-      name: 'Canning Town',
-      command: 'cgt',
-      stopPoint: '940GZZDLCGT',
-      platforms: ['Platform 1', 'Platform 3']
-    },
-    {
-      name: 'Canary Wharf',
-      command: 'cw',
-      stopPoint: '940GZZDLCAN',
-      platforms: ['Platform 3']
-    },
-    {
-      name: 'Canary Wharf Tube',
-      command: 'tcw',
-      stopPoint: '940GZZLUCYF',
-      platforms: ['Eastbound - Platform 2']
-    },
-    {
-      name: 'Stratford central line',
-      command: 'stc',
-      stopPoint: '940GZZLUSTD',
-      platforms: ['Eastbound - Platform 6', 'Westbound - Platform 3']
-    },
-    {
-      name: 'Stratford dlr',
-      command: 'std',
-      stopPoint: '940GZZDLSTD',
-      platforms: ['4a', '4b']
-    },
-    {
-      name: 'Leytonstone',
-      command: 'ley',
-      stopPoint: '940GZZLULYS',
-      platforms: ['Eastbound - Platform 3', 'Westbound - Platform 1', 'Westbound - Platform 2']
-    },
-    {
-      name: 'Westferry',
-      command: 'wfr',
-      stopPoint: '940GZZDLWFE',
-      platforms: ['Platform 1']
-    }
-  ]
+  {
+    name: "Poplar",
+    command: "pop",
+    stopPoint: "940GZZDLPOP",
+    platforms: ["Platform 1"]
+  },
+  {
+    name: "Ponton Dock",
+    command: "pdk",
+    stopPoint: "940GZZDLPDK",
+    platforms: ["Platform 2"]
+  },
+  {
+    name: "Canning Town",
+    command: "cgt",
+    stopPoint: "940GZZDLCGT",
+    platforms: ["Platform 1", "Platform 3"]
+  },
+  {
+    name: "Canary Wharf",
+    command: "cw",
+    stopPoint: "940GZZDLCAN",
+    platforms: ["Platform 3"]
+  },
+  {
+    name: "Canary Wharf Tube",
+    command: "tcw",
+    stopPoint: "940GZZLUCYF",
+    platforms: ["Eastbound - Platform 2"]
+  },
+  {
+    name: "Stratford central line",
+    command: "stc",
+    stopPoint: "940GZZLUSTD",
+    platforms: ["Eastbound - Platform 6", "Westbound - Platform 3"]
+  },
+  {
+    name: "Stratford dlr",
+    command: "std",
+    stopPoint: "940GZZDLSTD",
+    platforms: ["4a", "4b"]
+  },
+  {
+    name: "Leytonstone",
+    command: "ley",
+    stopPoint: "940GZZLULYS",
+    platforms: [
+      "Eastbound - Platform 3",
+      "Westbound - Platform 1",
+      "Westbound - Platform 2"
+    ]
+  },
+  {
+    name: "Westferry",
+    command: "wfr",
+    stopPoint: "940GZZDLWFE",
+    platforms: ["Platform 1"]
+  },
+];
 
-  module.exports = STATIONS
+PIERS = [
+  {
+    stopPoint: "930GCAW",
+    name: "Canary Wharf Pier",
+    command: "pier_cw",
+    direction: ["outbound"],
+    timeToStation: 4.5 * 60, // 4:30
+  },
+  {
+    stopPoint: "930GWRF",
+    name: "Royal Wharf Pier",
+    command: "pier_rw",
+    direction: ["inbound"],
+    timeToStation: 9 * 60, // 9:00
+  }
+]
+
+module.exports = { STATIONS, PIERS };
